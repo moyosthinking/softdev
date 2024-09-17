00f5145d71501140e8403750c6764ffbfb08d14d
Moyo Fagbuyi <rfagbuyi@gmail.com>
2024-09-17 11:42:54 -0400

    nothing but confusion i am not driver

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,27 +1,9 @@
-import random
-
 # Moyo Fagbuyi, Margie Cao, Tim Ng
 # Systems Level Programming
 # SoftDev
 # K04 -- Python dictionairies and random selection
 # 2024-09-13
 # time spent: 40/60
-
-
-krewes = {
-           4: [ 
-		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
-		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
-		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
-		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
-		],
-           5: [ 
-                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
-                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
-                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
-                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
-              ]
-         }
 
 def pop():
     keys = list(krewes.keys())
@@ -36,11 +18,20 @@ def pop():
     # y -> random int to get random student
     print(vals[y])
     
-    
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+ducks = {4:[], 5:[]}
 
-#tests
-pop()
-pop()
-pop()
-
-    
+pd$$$devo$$$ducky@@@pd$$$devo$$$ducky@@@pd$$$devo$$$ducky@@@
