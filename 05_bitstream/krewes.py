19868fa4d8a99da262c79827561f006e31ae34d3
Moyo Fib <rfagbuyi@gmail.com>
2024-09-17 21:31:03 -0400

    done but py not readin the text file

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,37 +1,28 @@
-# Moyo Fagbuyi, Margie Cao, Tim Ng
+#Moyo Fagbuyi, Tim Ng, Margie Cao
 # Systems Level Programming
 # SoftDev
-# K04 -- Python dictionairies and random selection
-# 2024-09-13
-# time spent: 40/60
+# K06 -- 
+# 2024-09-17
+# time spent: 1
 
-def pop():
-    keys = list(krewes.keys())
-    #keys -> lists of period
-    keysLen = len(keys)
-    x = random.randint(0, keysLen - 1)
-    #x -> random int to get a random period
-    vals = list(krewes.get(keys[x]))
-    # vals -> list of students from the period
-    valsLen = len(vals)
-    y = random.randint(0, valsLen - 1)
-    # y -> random int to get random student
-    print(vals[y])
+import random
+
+def duckies(str):
+    devos = str.split("@@@")
+    devos.remove("")
+    info = []
+    dict = {"pd": 1, "devo": "temp", "ducky": "temp"}
+    listdict = []
+    for x in range(len(devos)):
+        info = info + devos[x].split("$$$")
+    for x in range(0, len(info), 3):
+        dict = {"pd": 1, "devo": "temp", "ducky": "temp"}
+        dict["pd"] = info[x]
+        dict["devo"] = info[x+1]
+        dict["ducky"] = info[x+2]
+        listdict.append(dict)
+    x = random.randint(0, len(listdict)-1)
+    print(listdict[x]["devo"] + " " + listdict[x]["pd"] + " " + listdict[x]["ducky"])
     
-krewes = {
-           4: [ 
-		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
-		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
-		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
-		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
-		],
-           5: [ 
-                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
-                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
-                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
-                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
-              ]
-         }
-ducks = {4:[], 5:[]}
-
-pd$$$devo$$$ducky@@@pd$$$devo$$$ducky@@@pd$$$devo$$$ducky@@@
+x = open("krewes.txt", "r")
+duckies(x.read())
