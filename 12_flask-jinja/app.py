d8b355ad281a0ff6319b5789c8f7e7077cf615a4
Moyo Fagbuyi <rfagbuyi@gmail.com>
2024-09-27 11:46:33 -0400

    read readme and start

---
+++ 12_flask-jinja/app.py
@@ -0,0 +1,48 @@
+# N/a
+# Moyo, Will, Daniel
+# SoftDev
+# Sep 2024
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Q0:
+<your response here>
+
+Q1:
+<your response here>
+
+Q2:
+<your response here>
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Q0: What will happen if you remove render_template from the following statement?
+# (log prediction before executing...)
+#~~~~~~~~~~~~~~~~~~~~~~~~~n a new directory in your workshop, save a copy of the starter kit for using flask (and its Jinja2 templating engine) to serve templated pages.
+As a team...~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+
+coll = [0,1,1,2,3,5,8]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Q1: Can all of your teammates confidently predict the URL to use to load this page?
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@app.route("/my_foist_template") 
+def test_tmplt():
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Q2: What is the significance of each argument? Simplest, most concise answer best.
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
