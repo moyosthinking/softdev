fe32230710d111ff214c5cc47049406c811305f5
Moyo Fib <rfagbuyi@gmail.com>
2024-10-16 16:34:19 -0400

    update

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,28 +1,41 @@
 # Margie Cao, Moyo Fagbuyi, Tim Ng
 # System Level Programming
+# K16 - working with sessions and cookies
 # SoftDev
 # 2024-10-09
-# o
+# Time Spent: 3 hours
 
 from flask import Flask, render_template, request, session #this one stores like verything
-import os
+import os 
 from utl import ants
 
-
 app = Flask(__name__)
+app.secret_key = ants.poop()
 
-@app.route("/",  methods=['GET', 'POST'])
+@app.route("/",  methods=['GET','POST'])
 def disp_loginpage():
+    if 'username' and 'password' in session: # will immediately send you to response page if already logged
+        name = session['username']
+        return render_template('response.html', name = name)
     return render_template( 'login.html' ) #renders homepage
 
 
-@app.route("/auth", methods=['GET', 'POST'])
+@app.route("/auth", methods=['POST'])
 def post():
-    session[ants.poop()] = request.args['username']
-#     return render_template('response.html', inputted=inputted)
-    return "poop"
+    session['username'] = request.form['username'] # saves username + password
+    session['password'] = request.form['password']
+    name = session['username']
+    return render_template('response.html', name = name)
 
+@app.route("/logout", methods = ['GET', 'POST'])
+def logout():
+    session.pop('username', None) # deletes username and password from cookies
+    session.pop('password', None)
+    return render_template('logout.html')
 
+@app.route("/homepage", methods = ['GET', 'POST'])
+def redirect():
+    return render_template('login.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
--- 16_flask-sessions/utl/ants.py
+++ 16_flask-sessions/utl/ants.py
@@ -1,8 +1,4 @@
-from flask import Flask, render_template, request, session #this one stores like verything
 import os
 
-app = Flask(__name__)
-
-def poop():
-    app.secret_key = os.urandom(32)
-    return app.secret_key
+def sessionKey:
+    return os.urandom(32)
